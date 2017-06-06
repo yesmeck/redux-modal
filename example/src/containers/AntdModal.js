a9cf23f84f4ae71c8f1367e3a62034cfd73b397e
Josh Story <jcs.gnoff@gmail.com>
2017-06-06 08:28:44 -0700

    react 15.5 support with prop-types dependency

--- example/src/containers/AntdModal.js
+++ example/src/containers/AntdModal.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Modal } from 'antd'
 import { connectModal } from 'redux-modal'
 
--- example/src/containers/BootstrapModal.js
+++ example/src/containers/BootstrapModal.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Button, Modal } from 'react-bootstrap'
 import { connectModal } from 'redux-modal'
 
