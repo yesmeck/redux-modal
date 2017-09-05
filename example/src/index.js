2738d0702dd0bdfce192571563ebeaf0adf091ad
Wei Zhu <yesmeck@gmail.com>
2017-09-05 16:38:32 +0800

    Use cra for example

--- example/src/index.js
+++ example/src/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
 import DevTools from './containers/DevTools';
 
 const logger = createLogger()
-const finalCreateStore = compose(
-  applyMiddleware(logger),
-  DevTools.instrument()
-)(createStore)
-const store = finalCreateStore(reducer)
+
+const store = createStore(
+  reducer,
+  {},
+  compose(
+    applyMiddleware(logger),
+    DevTools.instrument()
+  )
+)
 
 render(
   <Provider store={store}>
